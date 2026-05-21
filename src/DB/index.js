c5546a7752a65d32cb5877f4c2816952920137a0
Randhir Kumar <veerrandhir3@gmail.com>
2026-05-22 00:12:01 +0530

    feat: implement database connection and add DB_Name export

--- src/DB/index.js
+++ src/DB/index.js
@@ -1,1 +1,17 @@
 import mongoose from "mongoose";
+import { DB_Name } from "../DB_Name/db_name.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/ ${DB_Name}`,
+    );
+    console.log("DB Connected :: You have || Now a db");
+  } catch (error) {
+    console.error("ERROR: connection failed ", error);
+    process.exit(1);
+  }
+};
+// console.log("db successfully connected", connectDB.connection.host);
+
+export default connectDB;
