df263bd12d57d74b79b28c0c76d5376b2864b502
Randhir Kumar <veerrandhir3@gmail.com>
2026-05-23 02:06:09 +0530

    refactor: streamline server initialization and error handling

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-import express from "express";
 import dotenv from "dotenv";
 import { DB_Name } from "./DB_Name/db_name.js";
 import connectDB from "./DB/index.js";
+import { app } from "./app.js";
+
 dotenv.config({
   path: "../.env",
 });
 
-const app = express();
-connectDB();
-
-app.get("/", (req, res) => {
-  res.send("hello world this is video server");
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running at port ${process.env.PORT}`);
-  app.on("error", (error) => {
-    console.error(" # ERROR :  While connecting with Server", error);
-    throw error;
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      // app.on("ERROR : Unable to connect with server");
+      console.log(":: Server is connected : Listning on :", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("ERROR : While creating server || Unable to connect ||");
   });
-  //   console.error("ERROR: Some thing went wrong while creating server");
-});
