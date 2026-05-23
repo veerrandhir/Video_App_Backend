319ce3c982abe72f39dbea54a5c4c02f618c5cb3
Randhir Kumar <veerrandhir3@gmail.com>
2026-05-23 08:45:33 +0530

    middleware like use cors, urlencoded, cookieParser added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 
 const app = express();
 app.get("/", (req, res) => {
   res.send("hello world this is video server");
 });
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: "14kb" }));
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(cookieParser());
+
 export { app };
