b93eda638a167336b29e9462215e4b8cf482e164
Randhir Kumar <veerrandhir3@gmail.com>
2026-05-25 15:50:15 +0530

    File Upload: multer file proper fn

---
+++ src/Middleware/multer.middleware.js
@@ -0,0 +1,39 @@
+import multer from "multer";
+import path from "node:path";
+
+const storage = multer.diskStorage({
+  // Step : 1 where you want to keep your file
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    const extension = path.extname(file.originalname); // TODO : Console.log file
+    cb(null, `${uniqueSuffix}${extension}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedMimeTypes = [
+    "video/mp4",
+    "video/mkv",
+    "video/avi",
+    "image/jpeg",
+    "image/png",
+  ];
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Unsupported file type"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 200 * 1024 * 1024, // 200 MB
+  },
+});
+
+module.exports = upload;
