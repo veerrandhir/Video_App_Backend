baf7400fa24dd4248ebbfa64752cbc9755ea5834
Randhir Kumar <veerrandhir3@gmail.com>
2026-05-25 22:35:09 +0530

    Added: injection of upload middleware to handle file on regiester event

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,12 +1,29 @@
 import { Router } from "express";
 import { registerUser } from "../Controller/user.controller.js";
 import express from "express";
+import { upload } from "../Middleware/multer.middleware.js";
+
 const router = Router();
 // no need to keep it in variable
 // this is ulternate way to write clean and clear code
 // Traditional method
 // app.get("userlogin", loginUsercontroller)
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  // before calling register user we inject multer middleware to upload files becauce we can automitaclly handle text info but to handle file we need middleware
+  upload.fields(
+    // no. of objects depends on how many files we required
+    // we are handling only two fils so need two object
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ),
+  registerUser,
+);
 
 export default router;
