a03e92ab35610df63a77b67438970951bc56240f
Randhir Kumar <veerrandhir3@gmail.com>
2026-05-26 10:14:29 +0530

    refactor: update multer storage configuration and simplify filename handling

--- src/Middleware/multer.middleware.js
+++ src/Middleware/multer.middleware.js
@@ -4,36 +4,15 @@ import path from "node:path";
 const storage = multer.diskStorage({
   // Step : 1 where you want to keep your file
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, path.resolve("src/DB/temp"));
   },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-    const extension = path.extname(file.originalname); // TODO : Console.log file
-    cb(null, `${uniqueSuffix}${extension}`);
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  const allowedMimeTypes = [
-    "video/mp4",
-    "video/mkv",
-    "video/avi",
-    "image/jpeg",
-    "image/png",
-  ];
-  if (allowedMimeTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error("Unsupported file type"), false);
-  }
-};
-
 const upload = multer({
   storage,
-  fileFilter,
-  limits: {
-    fileSize: 200 * 1024 * 1024, // 200 MB
-  },
 });
 
 export { upload };
