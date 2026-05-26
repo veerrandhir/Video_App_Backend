993d9c755763d0d9b774f3a339abdc3e19ce12b7
Randhir Kumar <veerrandhir3@gmail.com>
2026-05-26 10:14:19 +0530

    fix: update user model to enforce avatar requirement and correct schema definitions

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -2,7 +2,6 @@ import mongoose, { Schema } from "mongoose";
 import { timeStamp } from "node:console";
 
 import bcrypt from "bcrypt";
-import { use } from "react";
 
 import jwt from "jsonwebtoken";
 
@@ -30,32 +29,32 @@ const userSchema = new Schema(
     },
     avatar: {
       type: String,
+      required: [true, "Missing avatar from user.model"],
+    },
+    coverImage: {
+      type: String, // Cloudinary link We don't store our image and video directly to db
       // required: true,
     },
-    thumbnail: {
-      type: String, // Cloudinary link We don't store our image and video directly to db
-      required: true,
-    },
-    watchHistory: {
-      // keep objId of video to track watchHistory of user
-      type: Sschem.Type.ObjectId,
-      ref: "Video",
-    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
     refreshToken: {
       // hold referesh token
       type: String,
     },
   },
-  { timeStamp: true }, // Require created and updated time to be stored
+  { timeStamps: true }, // Require created and updated time to be stored
 );
 // Remember here we will provide simple fn to pre as we need to use this
 // arrow fn does'nt have it's this fn
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   // we passed next as params  sa it is a middleware
-  if (!this.modified("password")) return next();
+  if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 // Build own method to check passoword for login
@@ -63,7 +62,7 @@ userSchema.pre("save", async function (next) {
 // this method adds isPasswordCorrect method to each and every object
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    // custom methods added to userSchema 
+  // custom methods added to userSchema
   return await bcrypt.compare(password, this.password);
 };
 
